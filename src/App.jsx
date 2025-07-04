9881ec34d32ae8cd2433058e8ab898c40bab72d6
unknown <gabrielfurlanfogaca@gmail.com>
2025-07-04 20:19:25 -0300

    ajuste em imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Navbar from './navbar';
-import Forca from './components/jogodaforca/Forca';
-import CampoMinado from './components/campominado/CampoMinado';
-import JogoDoMilhao from './components/jogodomilhao/JogoDoMilhao';
+import Navbar from '../src/components/navbar';
+import Forca from '../src/components/jogodaforca/Forca';
+import CampoMinado from '../src/components/campominado/CampoMinado';
+import JogoDoMilhao from '../src/components/jogodomilhao/JogoDoMilhao';
 
 export default function App() {
   const [jogoAtivo, setJogoAtivo] = useState('forca'); // 'forca' | 'campominado' | 'jogodomilhao'
